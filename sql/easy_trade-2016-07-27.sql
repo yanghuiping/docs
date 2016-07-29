f8ab9b19768d915381e1e28099e259d8736c6001
yanghuiping <yanghuiping@91xcm.com>
2016-07-29 09:37:30 +0800

    update table

--- sql/easy_trade-2016-07-27.sql
+++ sql/easy_trade-2016-07-27.sql
@@ -1,58 +1,58 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2016/7/27 16:02:56                           */
+/* Created on:     2016/7/29 9:33:44                            */
 /*==============================================================*/
 
 
-drop table if exists purchase_contract;
+DROP TABLE IF EXISTS purchase_contract;
 
-drop table if exists purchase_product;
+DROP TABLE IF EXISTS purchase_product;
 
 /*==============================================================*/
 /* Table: purchase_contract                                     */
 /*==============================================================*/
-create table purchase_contract
+CREATE TABLE purchase_contract
 (
-   id                   numeric(8,0) not null comment '主键ID，自动生成',
-   contract_name        varchar(100) not null comment '合同名称',
-   contract_code        varchar(50) not null comment '合同编号',
-   buy_company_name     varchar(100) comment '需方公司名称',
-   buy_contacts_name    varchar(50) comment '需方联系人姓名',
-   buy_contacts_tel     varchar(15) comment '需方联系人电话',
-   sell_company_name    varchar(100) comment '供方公司名称',
-   sell_contacts_name   varchar(50) comment '供方联系人姓名',
-   sell_contacts_tel    varchar(15) comment '供方联系人电话',
-   create_time          datetime not null comment '添加时间',
-   effective_time       datetime comment '合同签订时间',
-   del_flag             numeric(8,0) not null comment '删除标识。0：非删除；1：删除',
-   update_time          datetime not null comment '更新时间',
-   remark               varchar(300) comment '备注',
-   primary key (id)
+   id                   INT NOT NULL AUTO_INCREMENT COMMENT '主键ID，自动生成',
+   contract_name        VARCHAR(100) NOT NULL COMMENT '合同名称',
+   contract_code        VARCHAR(50) NOT NULL COMMENT '合同编号',
+   buy_company_name     VARCHAR(100) COMMENT '需方公司名称',
+   buy_contacts_name    VARCHAR(50) COMMENT '需方联系人姓名',
+   buy_contacts_tel     VARCHAR(15) COMMENT '需方联系人电话',
+   sell_company_name    VARCHAR(100) COMMENT '供方公司名称',
+   sell_contacts_name   VARCHAR(50) COMMENT '供方联系人姓名',
+   sell_contacts_tel    VARCHAR(15) COMMENT '供方联系人电话',
+   create_time          DATETIME NOT NULL COMMENT '添加时间',
+   effective_time       DATETIME COMMENT '合同签订时间',
+   del_flag             NUMERIC(8,0) NOT NULL COMMENT '删除标识。0：非删除；1：删除',
+   update_time          DATETIME NOT NULL COMMENT '更新时间',
+   remark               VARCHAR(300) COMMENT '备注',
+   PRIMARY KEY (id)
 );
 
-alter table purchase_contract comment '采购合同表';
+ALTER TABLE purchase_contract COMMENT '采购合同表';
 
 /*==============================================================*/
 /* Table: purchase_product                                      */
 /*==============================================================*/
-create table purchase_product
+CREATE TABLE purchase_product
 (
-   id                   numeric(8,0) not null comment '主键ID，自动生成',
-   contract_id          numeric(8,0) not null comment '合同主键',
-   product_model        varchar(50) comment '型号',
-   product_name         varchar(100) comment '商品名称',
-   product_specifications varchar(50) comment '规格',
-   per_value            varchar(15) comment '1单位数量',
-   unit                 varchar(100) comment '单位',
-   num                  varchar(50) comment '数量',
-   price                varchar(15) comment '单价',
-   total_amount         datetime comment '总金额',
-   effective_time       datetime not null comment '创建时间',
-   update_time          datetime not null comment '更新时间',
-   del_flag             numeric(8,0) not null comment '删除标识。0：非删除；1：删除',
-   remark               varchar(300) comment '备注',
-   primary key (id)
+   id                   INT NOT NULL AUTO_INCREMENT COMMENT '主键ID，自动生成',
+   contract_id          NUMERIC(8,0) NOT NULL COMMENT '合同主键',
+   product_model        VARCHAR(50) COMMENT '型号',
+   product_name         VARCHAR(100) COMMENT '商品名称',
+   product_specifications VARCHAR(50) COMMENT '规格',
+   per_value            VARCHAR(15) COMMENT '1单位数量',
+   unit                 VARCHAR(100) COMMENT '单位',
+   num                  VARCHAR(50) COMMENT '数量',
+   price                VARCHAR(15) COMMENT '单价',
+   total_amount         DATETIME COMMENT '总金额',
+   effective_time       DATETIME NOT NULL COMMENT '创建时间',
+   update_time          DATETIME NOT NULL COMMENT '更新时间',
+   del_flag             NUMERIC(8,0) NOT NULL COMMENT '删除标识。0：非删除；1：删除',
+   remark               VARCHAR(300) COMMENT '备注',
+   PRIMARY KEY (id)
 );
 
-alter table purchase_product comment '采购产品表';
+ALTER TABLE purchase_product COMMENT '采购产品表';
 
